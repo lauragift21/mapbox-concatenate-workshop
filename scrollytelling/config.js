881aa2a16504eabbd19f341ece8ee07dc77482ca
lauragift21 <laurandidi21@gmail.com>
2019-10-09 15:35:07 +0100

    update map

--- scrollytelling/config.js
+++ scrollytelling/config.js
@@ -24,6 +24,62 @@ var config = {
               "bearing": 136.79999999999995
             }
         },
+         {
+          id: 'Concatenate-Kenya',
+          location: {
+            "center": [
+              38.65729400711922,
+              -4.261468468040192
+            ],
+            "zoom": 6.3060616689703503,
+            "pitch": 0,
+            "bearing": 0
+          },
+          title: "Concatenate Kenya Trip",
+          description: "I'll be in Nairobi Kenya for Concatanate Conference from Oct 15th"
+        },
+        {
+          id: 'microsoft-ignite',
+          location: {
+            "center": [
+              -79.06782614568283,
+              29.089693898945995
+            ],
+            "zoom": 6.3060616689703503,
+            "pitch": 0,
+            "bearing": 0
+          },
+          title: "Microsoft Ignite 2019",
+          description: "Super excited to visit Orlando Florida for Microsoft Ignite as a commmunity member."
+        },
+        {
+          id: 'aws-reinvent',
+          location: {
+          "center": [
+            -95.56071737481858,
+            30.56973346889653
+          ],
+          "zoom": 6.3060616689703503,
+          "pitch": 0,
+          "bearing": 0
+        },
+        title: "AWS Re:invent 2019",
+        description: "I'll be attending the annual AWS Re:invent event in the US. "
+        },
+        {
+          id: 'vacation-bali',
+          location: {
+            "center": [
+              117.10834586290457,
+              -7.539779710681543
+            ],
+            "zoom": 6.6706852767638418,
+            "pitch": 0,
+            "bearing": 0
+          },
+          title: "Vacation 2021",
+          description: "Next year, One place I'll love to visit is definetly Bali so this will be my vacation goal!"
+        }
         //TODO Copy-paste more chapters here - one for each point in your dataset!
     ]
 };
